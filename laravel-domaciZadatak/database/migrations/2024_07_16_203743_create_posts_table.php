007330494961edee020013e658dae14e5941d066
Li9st <lidijastankovic476@gmail.com>
2024-07-23 17:34:48 +0200

    Ispravka baga

--- laravel-domaciZadatak/database/migrations/2024_07_16_203743_create_posts_table.php
+++ laravel-domaciZadatak/database/migrations/2024_07_16_203743_create_posts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title',100);
-            $table->text('content',200);
-            $table->text('extra',200);
+            $table->text('content');
+            $table->text('extra');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
