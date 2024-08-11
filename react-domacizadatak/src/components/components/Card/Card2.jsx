405b2470023bf7254c113a69760458c056fec85e
Li9st <lidijastankovic476@gmail.com>
2024-08-11 13:59:10 +0200

    Dodavanje dodatnih komponenti za pravljenje drag and drop sajtova

---
+++ react-domacizadatak/src/components/components/Card/Card2.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Card2 extends Component {
+  render() {
+    return <div>Card2</div>;
+  }
+}
+
+export default Card2;
---
+++ react-domacizadatak/src/components/components/Heading/Paragraph.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Paragraph extends Component {
+  render() {
+    return <div>Paragraph</div>;
+  }
+}
+
+export default Paragraph;
