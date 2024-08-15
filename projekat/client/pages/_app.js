11d5e837ece57a05960311b3efaec70d7acc4edc
JelenaVujicic <jelena.vujicic0711@gmail.com>
2024-08-15 15:22:50 +0200

    Implementacija menija za administratora

--- projekat/client/pages/_app.js
+++ projekat/client/pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "../context/theme";
 // import "antd/dist/antd.css";
 // import "antd/dist/antd.dark.css";
+import TopNav from "../components/TopNav";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <TopNav />
       <Component {...pageProps} />
     </ThemeProvider>
   );
